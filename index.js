45b15adb1439f0803ebe233333fb5bfa1e6527af
masum <masumcit44@gmail.com>
2023-06-08 10:26:49 +0600

    slider data updated

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  // console.log(authorization);
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    // console.log(decoded);
+    req.decoded = decoded; // just next e pass korar jonno
+    next();
+  });
+};
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bs8dc9c.mongodb.net/?retryWrites=true&w=majority`;
@@ -23,6 +47,45 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const usersCollection = client.db("combatDB").collection("users");
+    const topSliderCollection = client.db("combatDB").collection("topslider")
+    //jwt
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // users related api
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const query = { email: user?.email };
+      const existingUser = await usersCollection.findOne(query)
+      if(existingUser){
+        return res.send({message: "user already existed"})
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result)
+    });
+
+    // top slider api
+
+    app.get("/topslider",async(req,res)=>{
+      const result = await topSliderCollection.find().toArray()
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
